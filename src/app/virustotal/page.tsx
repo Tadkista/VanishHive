8f8c0644a3755af53229e12d93e4b926cadfb394
Tadkista <wiktor01t@gmail.com>
2025-03-10 12:14:36 +0100

    Update page.tsx

--- src/app/virustotal/page.tsx
+++ src/app/virustotal/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from "../navbar";
 
-import { useEffect } from 'react';
+
  if (typeof window !== 'undefined') {
 localStorage.setItem("virusTotalApiKey", "ebc3aa96b22e1cb1c1223acb0c72ddc09f766720cdfc580f0b514ec136161b5a")
  }
