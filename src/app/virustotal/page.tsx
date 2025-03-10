be0977129150ff3e558200da26deada99b688c9d
Tadkista <wiktor01t@gmail.com>
2025-03-10 12:01:10 +0100

    Update page.tsx

--- src/app/virustotal/page.tsx
+++ src/app/virustotal/page.tsx
@@ -111,7 +111,7 @@ const VirusTotalScanner: React.FC = () => {
     if (!apiKey.trim()) {
       //setError('API key is required');
       //return false;
-      apiKey = ebc3aa96b22e1cb1c1223acb0c72ddc09f766720cdfc580f0b514ec136161b5a;
+      apiKey = "ebc3aa96b22e1cb1c1223acb0c72ddc09f766720cdfc580f0b514ec136161b5a";
       return true;
     }
 
