2c95fca25f2650186fd4711b0b7f7410d392e98e
Tadkista <wiktor01t@gmail.com>
2025-03-10 12:08:27 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "./footer";
 import Landing from "./landing";
 import Navbar from "./navbar";
-
+localStorage.setItem("virusTotalApiKey", "ebc3aa96b22e1cb1c1223acb0c72ddc09f766720cdfc580f0b514ec136161b5a")
 export default function Home() {
   return (
     <>
